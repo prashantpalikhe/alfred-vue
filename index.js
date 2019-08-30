059da54211a26e1a223316dca60bf3683b501fb2
Prashant Palikhe <prashpal09@gmail.com>
2019-08-30 11:30:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const alfy = require('alfy');
+const algoliasearch = require('algoliasearch');
+const { last } = require('lodash');
+
+const client = algoliasearch('BH4D9OD16A', '85cc3221c9f23bfbaa4e3913dd7625ea');
+const index = client.initIndex('vuejs');
+
+(async () => {
+	const { hits } = await index.search({
+		query: alfy.input,
+		hitsPerPage: 5
+	});
+
+	const output = hits.map(hit => {
+		const result = {
+			title: hit.anchor,
+			subtitle: hit.anchor,
+			arg: hit.url,
+			quicklookurl: hit.url
+		};
+
+		if (hit.hierarchy) {
+			const hierarchies = Object.values(hit.hierarchy).filter(Boolean);
+
+			result.title = last(hierarchies);
+			result.subtitle = hierarchies.join(' > ');
+		}
+
+		return result;
+	});
+
+	alfy.output(output);
+})();
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-vue [![Build Status](https://travis-ci.org/prashantpalikhe/alfred-vue.svg?branch=master)](https://travis-ci.org/prashantpalikhe/alfred-vue)
+
+> Alfred workflow for Vue documentation
+
+
+## Install
+
+```
+$ npm install --global alfred-vue
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `vue`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Prashant Palikhe](https://twitter.com/PrashantPalikhe)
